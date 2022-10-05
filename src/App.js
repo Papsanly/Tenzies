758958943df6935ee08453dd30bbb0925cf3d480
Papsan <papsanlysenko@gmail.com>
2022-10-05 20:42:04 +0200

    added ability to win

--- src/App.js
+++ src/App.js
@@ -24,39 +24,61 @@ export default function App() {
     }
 
     const [dice, setDice] = React.useState(RollDice())
+    const [isGameWon, setIsGameWon] = React.useState(false)
+
+    React.useEffect(() => {
+        if (
+            dice.every(die => die.value === dice[0].value) &&
+            dice.every(die => die.isHeld)
+        ) {
+            console.log("You win!")
+            setIsGameWon(true)
+        }
+    }, [dice])
 
     function handleDiceClick(event) {
-        setDice(dice => {
-            return dice.map(die => {
-                if (die.id === event.target.id)
-                    die.isHeld = !die.isHeld
-                return die
+        if (!isGameWon)
+            setDice(dice => {
+                return dice.map(die => {
+                    if (die.id === event.target.id)
+                        die.isHeld = !die.isHeld
+                    return die
+                })
             })
-        })
     }
 
     function handleRollButtonClick() {
-        setDice(dice => ReRollDice(dice))
+        if (isGameWon) {
+            setDice(RollDice())
+            setIsGameWon(false)
+        }
+        else
+            setDice(dice => ReRollDice(dice))
     }
 
+    const headerStyle = isGameWon ? "you-win-title" : "you-win-title transparent"
+
     return (
-        <main>
-            <h1 className="title">Tenzies</h1>
-            <p className="text">
-                Roll until all dice are the same.
-                Click each die to freeze it at its current
-                value between rolls.
-            </p>
-            <DiceContainer
-                dice={dice}
-                onClick={handleDiceClick}
-            />
-            <button
-                className="roll-button"
-                onClick={handleRollButtonClick}
-            >
-                Roll
-            </button>
-        </main>
+        <div>
+            <header className={headerStyle}>You Win!</header>
+            <main>
+                <h1 className="title">Tenzies</h1>
+                <p className="text">
+                    Roll until all dice are the same.
+                    Click each die to freeze it at its current
+                    value between rolls.
+                </p>
+                <DiceContainer
+                    dice={dice}
+                    onClick={handleDiceClick}
+                />
+                <button
+                    className="roll-button"
+                    onClick={handleRollButtonClick}
+                >
+                    {isGameWon ? "Reset dice" : "Roll"}
+                </button>
+            </main>
+        </div>
     )
 }
